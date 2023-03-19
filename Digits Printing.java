157e3c76a6b3466bf8eea858d20d3744016e05d5
chandan kumar <chandankumarmech18@gmail.com>
2023-03-19 17:53:47 +0530

    digits printing

---
+++ Digits Printing.java
@@ -0,0 +1,17 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        // Scanner sc= new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
+        String s = sc.next();
+        char a = s.charAt(1);
+        char b = s.charAt(2);
+        System.out.println(a+""+b);
+    
+    }
+}
