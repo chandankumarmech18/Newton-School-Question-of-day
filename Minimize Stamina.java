98190a97c652e354a3687d7f7e741aa547f2460a
chandan kumar <chandankumarmech18@gmail.com>
2023-03-28 08:33:39 +0530

    minimize stamina

---
+++ Minimize Stamina.java
@@ -0,0 +1,28 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = scanner.nextInt();
+        }
+        int p = Math.round((float) sum(x) / n);
+        int stamina = 0;
+        for (int i = 0; i < n; i++) {
+            stamina += (x[i] - p) * (x[i] - p);
+        }
+        System.out.println(stamina);
+    }
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
