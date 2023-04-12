b96e41a3dc44a682bb47af5fe45acf3653ab797c
chandan kumar <chandankumarmech18@gmail.com>
2023-04-12 12:40:11 +0530

    condition check

---
+++ Condition Check.java
@@ -0,0 +1,31 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int[] arr= new int[n];
+        int[] brr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            brr[i] = sc.nextInt();
+        }
+
+        int sum =0;
+        
+        for(int i=0;i<n;i++){
+            sum = sum + (arr[i]*brr[i]);
+        }
+        if(sum==0)
+        System.out.println("Yes");
+        else
+        System.out.println("No");
+
+    }
+}
